a0ee2ad2f5538e4a3d963324ad4442eaffaaa0a2
Shachar Itzhaky <corwin.amber@gmail.com>
2021-11-04 17:49:27 +0200

    added the frontend code

---
+++ ui/main.js
@@ -0,0 +1,55 @@
+
+const Board = {
+  data() {
+    return {
+      num_cols:16,
+      num_rows:16,
+      list_walls:[]
+    }
+  },
+  watch: {
+     list_walls: {
+        deep: true,
+        handler(newValue) {
+           localStorage['turtle-board'] = JSON.stringify({walls: newValue});
+        }
+     } 
+  },
+  methods: {
+    wall(x,y){
+        if (this.isWall(x,y)) {
+           var l = this.list_walls.filter(([px,py]) => {
+                return !((x == px) && (y == py))
+            })
+         this.list_walls = l        
+         }    
+        else 
+            this.list_walls.push([x,y])  
+    },
+    isWall(x,y) {
+        //some returns true iff [x,y] is in the list
+        return this.list_walls.some(([px,py]) => {
+            // [x,y] == [px,py]  <<< wrong!! in java strong
+            return (x == px) && (y == py);
+        })
+    },
+    clear() {
+        this.list_walls = [];
+    }
+  }
+}
+
+var app = Vue.createApp(Board).mount('#Board')
+
+var oldState = JSON.parse(localStorage['turtle-board'] || '{}');
+if (oldState.walls)
+    app.list_walls = oldState.walls;
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
